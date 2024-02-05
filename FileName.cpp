d661125d04752750d938ac67bb06081f77f16dde
ValeriaRabch <rabchenukv@gmail.com>
2024-02-05 18:33:29 +0200

    Add create massives

--- FileName.cpp
+++ FileName.cpp
@@ -6,7 +6,10 @@ using namespace std;
 
 
 int main() {
-	
+	const int Size = 1000;
+	char str[Size]; cin.getline(str, Size);
+	char str2[50]; cin.getline(str2, 50);
+	char str3[50]; cin.getline(str3, 50);
 
 	return 0;
 }
